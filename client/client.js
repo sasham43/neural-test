7f19af787f07033fdc4b806369eeb65b9d8d7812
sasham43 <sashasemail@gmail.com>
2016-09-14 12:02:54 -0500

    neural net learns to paint chrome :)

--- client/client.js
+++ client/client.js
@@ -3,43 +3,69 @@ angular.module('neuralApp', []);
 angular.module('neuralApp').controller('MainController', ['$http', function($http){
   var mc = this;
 
-  var outputs = [];
+  var perceptron = null;
+  var worker = null; // ?
+  var index = 0; // ?
+  var image_data = null;
+  var canvas = null;
+  var context = null;
+  var size = 125 * 125;
+  var iteration = 0; // ?
+  var to = null; // ?
+  var px = null;
 
-  var perceptron = new synaptic.Architect.Perceptron(2,3,1);
+  var getData = function(imageObj){
+    canvas = canvas || document.getElementById('canvas-demo');
+    context = context || canvas.getContext('2d');
 
-  var results = perceptron.trainer.XOR({
-    iterations: 100000,
-    error: 0.0001,
-    rate: 1
-  });
-
-  var validate = function(){
-    outputs = [];
-    outputs.push({
-      input: '0 0',
-      output: perceptron.activate([0,0])[0].toFixed(3),
-      target: 0
-    });
-    outputs.push({
-      input: '0 1',
-      output: perceptron.activate([0,1])[0].toFixed(3),
-      target: 1
-    });
-    outputs.push({
-      input: '1 0',
-      output: perceptron.activate([1,0])[0].toFixed(3),
-      target: 1
-    });
-    outputs.push({
-      input: '1 1',
-      output: perceptron.activate([1,1])[0].toFixed(3),
-      target: 0
-    });
-
-    console.log(outputs);
+    context.drawImage(imageObj, 0, 0);
+    var imageData = context.getImageData(0, 0, 125, 125);
+    return imageData.data;
   };
 
-  validate();
+  var train = mc.train = function(){
+    perceptron = new synaptic.Architect.Perceptron(2, 15, 3);
+    image_data = getData(document.getElementById('input'));
+    console.log('training...');
+    paint();
+  };
+
+  var iterate = function(){
+    for(var x = 0; x < 125; x++){
+      for(var y = 0; y < 125; y++){
+        var dynamicRate = .01/(1+0.0005*iteration);
+        px = pixel(input, x, y);
+        perceptron.activate([x/125, y/125]);
+        perceptron.propagate(dynamicRate, pixel(image_data, x, y));
+      }
+    }
+    paint();
+  };
+
+  var pixel = function(data, x, y){
+    var red = data[((125 * y) + x) * 4];
+    var green = data[((125 * y) + x) * 4 + 1];
+    var blue = data[((125 * y) + x) * 4 + 2];
+    return [red/255, green/255, blue/255];
+  };
+
+  var paint = function(){
+    console.log('painting...');
+    var imageData = context.getImageData(0,0,125,125);
+    for (var x = 0; x < 125; x++){
+      for (var y = 0; y < 125; y++){
+        var rgb = perceptron.activate([x/125,y/125]);
+        imageData.data[((125 * y) + x) * 4] = (rgb[0] )* 255;
+				imageData.data[((125 * y) + x) * 4 + 1] = (rgb[1] ) * 255;
+				imageData.data[((125 * y) + x) * 4 + 2] = (rgb[2] ) * 255;
+      }
+    }
+    context.putImageData(imageData,0,0);
+
+    requestAnimationFrame(iterate);
+  };
+
+  // iterate();
 
   console.log('main controller loaded.');
 }]);
